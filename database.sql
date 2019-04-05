09cbb789aa6011d7424ffbb2932d4b692b05796b
JASEpstein <jasepstein@gmail.com>
2019-04-04 20:47:09 -0500

    Basic file structure

---
+++ database.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS greatbay_db;
+
+CREATE DATABASE greatbay_db;
+USE greatbay_db;
+
+CREATE TABLE items
+(
+    id INTEGER(10)
+    AUTO_INCREMENT not null,
+    itemName VARCHAR
+    (30) null,
+    startBid INTEGER
+    (10) null,
+    primary key
+    (id)
+);
+
+
+
