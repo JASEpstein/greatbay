bdc6fa2678ef45d27528bc44a0c82d670091b5de
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-04-04 21:11:04 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Load the NPM Package inquirer
 var inquirer = require("inquirer");
+var mysql = require("mysql");
 
 // Create a "Prompt" to ask them for a letter - ex. basic.js
 inquirer.prompt([
@@ -16,7 +17,10 @@ inquirer.prompt([
       }
 ]).then(function(inquirerResponse) {
     if (inquirerResponse.choices === "Post") {
-        console.log("you choose " + inquirerResponse.whatToDo.choices);
+        console.log("Please enter your item & starting bid: ");
+        var item = process.argv[2];
+        var startingBid = process.argv[3];
+        INSERT INTO items SET
     } else {
         console.log("you choose " + inquirerResponse.whatToDo.choices);
     }
